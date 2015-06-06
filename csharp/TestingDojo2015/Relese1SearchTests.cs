f0cd39f1626d08df2e55752132f3c6520289d3a6
duken <duken@fake.com>
2015-06-06 12:19:02 +0600

    Relese2 add new tests

---
+++ csharp/TestingDojo2015/Relese1SearchTests.cs
@@ -0,0 +1,101 @@
+﻿namespace TestingDojo2015
+{
+    #region using
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+
+    #endregion
+
+    [TestFixture]
+    public class SearchTests : BaseTestFixture
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void SearchById()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("3");
+
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SearchByIdSubmit()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("3");
+            searchString.Submit();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SearchByName()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("Монитор");
+
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void SearchByNameWithoutReg()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("монитор");
+
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void SearchBySubStringName()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("s");
+
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(6));
+        }
+
+        #endregion
+    }
+}
---
+++ csharp/TestingDojo2015/Relese2AddTests.cs
@@ -0,0 +1,94 @@
+﻿namespace TestingDojo2015
+{
+    #region using
+
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+
+    #endregion
+
+    [TestFixture]
+    public class Relese2AddTests : BaseTestFixture
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void AddNewItem()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var addButton = mainWindow.FindElement(By.Id("AddNewProductMW"));
+            addButton.Click();
+            
+
+            var addNewItemWindow = this.Driver.FindElementById("AddNewProductWindow");
+
+            var addQuery = addNewItemWindow.FindElement(By.Id("NameAW"));
+            addQuery.SendKeys("new Item");
+
+            addNewItemWindow.FindElement(By.Id("AddAW")).Click();
+
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void NewItemIdIsMaxPlusOne()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var addButton = mainWindow.FindElement(By.Id("AddNewProductMW"));
+            addButton.Click();
+
+
+            var addNewItemWindow = this.Driver.FindElementById("AddNewProductWindow");
+
+            var addQuery = addNewItemWindow.FindElement(By.Id("NameAW"));
+            addQuery.SendKeys("new Item");
+
+            addNewItemWindow.FindElement(By.Id("AddAW")).Click();
+
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            var row = productItems.Last().FindElements(By.ClassName("TextBlock"));
+            
+            Assert.That(row.First().GetAttribute("Name"), Is.EqualTo("9"));
+        }
+
+
+        [Test]
+        public void NewItemCancel()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var addButton = mainWindow.FindElement(By.Id("AddNewProductMW"));
+            addButton.Click();
+
+
+            var addNewItemWindow = this.Driver.FindElementById("AddNewProductWindow");
+
+            var addQuery = addNewItemWindow.FindElement(By.Id("NameAW"));
+            addQuery.SendKeys("new Item");
+
+            addNewItemWindow.FindElement(By.Id("CancelAW")).Click();
+
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+
+            Assert.That(productItems.Count, Is.EqualTo(8));
+        }
+
+
+
+        #endregion
+    }
+}
